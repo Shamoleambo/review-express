2989c81be8bf055bec35582fb901f93b0aed3d55
Tiago Gomes <tidgomes@gmail.com>
2023-03-30 10:45:11 -0300

    seve a public folder

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000, () => {
   console.log('App is running')
